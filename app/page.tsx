4fba6429f41573fee75989881f0fdd90bcf9a9f6
Suryakant <skant758@gmail.com>
2024-06-29 15:06:26 +0530

    fix: prisma schema

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
 import prisma from "@/lib/db";
+import { redirect } from "next/navigation";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+
 export default async function Home() {
 	const book = await prisma.book.findMany();
 	return (
 		<main className=''>
-			<div>
+			<article>
 				{book.map((item, index) => (
 					<div key={index}>
 						<h1>{item.title}</h1>
-						<h2>{item.content}</h2>
+						<h2>{item.description}</h2>
 					</div>
 				))}
-			</div>
+			</article>
 		</main>
 	);
 }
